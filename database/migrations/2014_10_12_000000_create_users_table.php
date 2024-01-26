23c33ad819e22d949790eb7bcd9621dc69efad10
aridane <aridanecabreraramirez@gmail.com>
2024-01-26 18:44:38 +0100

    actualizado

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -22,6 +22,9 @@ return new class extends Migration
             $table->string('github_id')->nullable();
             $table->string('github_token')->nullable();
             $table->string('github_refresh_token')->nullable();
+            $table->string('google_id')->nullable();
+
+
         });
     }
 
